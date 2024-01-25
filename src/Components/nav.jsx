5173ed97c7f9c93423f8880e43fdc920c46b1ca1
Ayush-kathayat <kathayatayush789@gmail.com>
2024-01-25 23:50:00 +0530

    add : added the dark and light mode btn

--- src/Components/nav.jsx
+++ src/Components/nav.jsx
@@ -5,34 +5,42 @@ import "./nav.css";
 const Nav = () => {
   return (
     <>
-      <nav>
-        <h1>Ayush Kathayat</h1>
-        <ul>
-          <ul>
-            <li>
+      <nav className="navbar">
+        <h1 className="navbar__logo">Ayush Kathayat</h1>
+
+        <ul className="links">
+          <li className="links__item">
             <img src="/home.svg" alt="Home" />
-              <Link to="/home">Home</Link>
-            </li>
-            <li>
-              <img src="/user.svg" alt="About" />
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <img src="/code.svg" alt="Project" />
-              <Link to="/projects">Projects</Link>
-            </li>
-            <li>
-              <img src="/file-text.svg" alt="Resume"/>
-              <Link to="/resume">Resume</Link>
-            </li>
-
-            <li>
-              <img src="/github.svg" alt="git-link"/>
-            </li>
-          </ul>
-
+            <Link to="/">Home</Link>
+          </li>
+          <li className="links__item">
+            <img src="/user.svg" alt="About" />
+            <Link to="/about">About</Link>
+          </li>
+          <li className="links__item">
+            <img src="/code.svg" alt="Project" />
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li className="links__item">
+            <img src="/file-text.svg" alt="Resume" />
+            <Link to="/resume">Resume</Link>
+          </li>
+          <li className="links__item">
+            <img src="/github.svg" alt="git-link" />
+          </li>
         </ul>
 
+        <div className="mode">
+          <button>
+            {" "}
+            <img src="/sun.svg" alt="light-mode" />
+          </button>
+
+          <button>
+            {" "}
+            <img src="/moon.svg" alt="dark-mode" />
+          </button>
+        </div>
       </nav>
     </>
   );

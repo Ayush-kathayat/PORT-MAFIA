64f28f961dd5c96bd4aee17afba153b94e093566
Ayush-kathayat <kathayatayush789@gmail.com>
2024-01-28 13:54:38 +0530

    add : navbar responsiveness is done

--- src/Components/nav.jsx
+++ src/Components/nav.jsx
@@ -1,15 +1,16 @@
 import { Link } from "react-router-dom";
-
+import { useState } from "react";
 import "./nav.css";
 
 const Nav = () => {
+  const [showMobMenu, setShowMobMenu] = useState(false);
 
   const gitProfile = () => {
     window.location.href = "https://github.com/Ayush-kathayat";
- };
+  };
   return (
     <>
-      <nav className="navbar">
+      <nav className={showMobMenu ? "active navbar" : "navbar"}>
         <h1 className="navbar__logo">Ayush Kathayat .</h1>
 
         <ul className="links">
@@ -67,6 +68,37 @@ const Nav = () => {
             </svg>
           </li>
         </ul>
+
+        <div
+          className="mobile-navbar-btn"
+          onClick={() => setShowMobMenu(!showMobMenu)}
+        >
+          <svg
+            className="open-menu"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 26 26"
+            width="26px"
+            height="26px"
+          >
+            <path
+              d="M 0 4 L 0 6 L 26 6 L 26 4 Z M 0 12 L 0 14 L 26 14 L 26 12 Z M 0 20 L 0 22 L 26 22 L 26 20 Z"
+              fill="#ffffff"
+            />
+          </svg>
+
+          <svg
+            className="close-menu"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 50 50"
+            width="28px"
+            height="28px"
+          >
+            <path
+              d="M 9.15625 6.3125 L 6.3125 9.15625 L 22.15625 25 L 6.21875 40.96875 L 9.03125 43.78125 L 25 27.84375 L 40.9375 43.78125 L 43.78125 40.9375 L 27.84375 25 L 43.6875 9.15625 L 40.84375 6.3125 L 25 22.15625 Z"
+              fill="#ffffff"
+            />
+          </svg>
+        </div>
       </nav>
     </>
   );

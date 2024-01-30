59f6a379d0ef8f481e8e7828b502f5b0906a4fea
Ayush-kathayat <kathayatayush789@gmail.com>
2024-01-30 13:23:28 +0530

    chore/add : hero section and about me section is added

--- src/pages/HomePage/home.jsx
+++ src/pages/HomePage/home.jsx
@@ -17,11 +17,33 @@ const Home = () => {
           </p>
 
           <AnimatedText texts={texts} />
-          
         </div>
         <div className="hero-right">
           <img src="/hero-img.svg" alt="hero" />
         </div>
+      </div>
+
+      <div className="about-me">
+        <h1>LET ME <span>INTRODUCE</span> MYSELF</h1>
+       <p className="para">
+          I fell for programming, and I have definitely picked up a thing or two
+          along the way. 🚀
+        </p>
+
+        <p className="para">
+          Proficient in the triumvirate of coding: <span>C++,JavaScript, and Python,</span>
+          crafting digital symphonies with each line. 🎵💻
+        </p>
+
+        <p className="para">
+          Fascinated by forging <span>new web technologies and products</span>, with a keen
+          eye on <span>blockchain innovation.</span> 🌐⛓️
+        </p>
+        <p className="para">
+          Diving into <span>product development</span> with fervor, leveraging <span>Node.js , Next.js</span> and
+          cutting-edge <span>JavaScript libraries</span> like <span>React.js</span> and <span>Next.js.</span> 🚀🔧
+        </p>
+       
       </div>
     </>
   );

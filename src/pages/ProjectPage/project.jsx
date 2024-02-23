d455ceafb89266831ebbc31b0f133fc9db9df01e
Ayush-kathayat <kathayatayush789@gmail.com>
2024-02-23 14:03:43 +0530

    add : added some more cards

--- src/pages/ProjectPage/project.jsx
+++ src/pages/ProjectPage/project.jsx
@@ -15,9 +15,39 @@ const Project = () => {
           githubLink="https://github.com/example"
           demoLink="https://example.com/demo"
         />
+
+        <Card
+          image="path/to/image.png"
+          title="Example Card"
+          paragraph="This is an example of a reusable card component."
+          githubLink="https://github.com/example"
+          demoLink="https://example.com/demo"
+        />
+
+        <Card
+          image="path/to/image.png"
+          title="Example Card"
+          paragraph="This is an example of a reusable card component."
+          githubLink="https://github.com/example"
+          demoLink="https://example.com/demo"
+        />
       </div>
 
       <div className="card-row">
+        <Card
+          image="path/to/image.png"
+          title="Example Card"
+          paragraph="This is an example of a reusable card component."
+          githubLink="https://github.com/example"
+          demoLink="https://example.com/demo"
+        />
+        <Card
+          image="path/to/image.png"
+          title="Example Card"
+          paragraph="This is an example of a reusable card component."
+          githubLink="https://github.com/example"
+          demoLink="https://example.com/demo"
+        />
         <Card
           image="path/to/image.png"
           title="Example Card"
